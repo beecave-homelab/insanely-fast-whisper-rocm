5e7542fa54e8a6024323aa6395d04e65b86b2915
Lowie <lowie@beecave.nl>
2024-08-28 14:25:11 +0200

    feat ✨: Added model selection feature.

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ DEFAULT_TRANSCRIPTS = os.getenv("TRANSCRIPTS", "/app/transcripts")
 DEFAULT_LOGS = os.getenv("LOGS", "/app/logs")
 DEFAULT_BATCH_SIZE = int(os.getenv("BATCH_SIZE", 4))
 DEFAULT_VERBOSE = os.getenv("VERBOSE", "true").lower() in ("true", "1", "yes")
+DEFAULT_MODEL = os.getenv("MODEL", "openai/whisper-large-v3")
 
 def setup_logging(verbose):
     log_level = logging.DEBUG if verbose else logging.INFO
@@ -34,7 +35,8 @@ def main():
     parser.add_argument('-l', '--logs', default=DEFAULT_LOGS, help='Directory to save log files')
     parser.add_argument('-b', '--batch-size', default=DEFAULT_BATCH_SIZE, type=int, help='Batch size for processing')
     parser.add_argument('-v', '--verbose', action='store_true', default=DEFAULT_VERBOSE, help='Enable verbose logging')
-    
+    parser.add_argument('-m', '--model', default=DEFAULT_MODEL, help='Model to be used for processing')
+
     args = parser.parse_args()
     
     setup_logging(args.verbose)
@@ -44,8 +46,6 @@ def main():
         if not os.path.isdir(directory):
             error_exit(f"Directory not found: {directory}")
 
-
-    
     while True:
         files_found = False
         no_new_files_logged = False
@@ -60,12 +60,11 @@ def main():
 
                 # Check if the transcript already exists
                 if os.path.exists(transcript_output):
-                    # logging.info(f"Skipping {filename}, transcript already exists.")
                     time.sleep(60)
                     continue
                 
                 # Construct the command
-                command = f"insanely-fast-whisper --file-name '{filepath}' --transcript-path '{transcript_output}' --batch-size '{args.batch_size}'"
+                command = f"insanely-fast-whisper --file-name '{filepath}' --model '{args.model}' --transcript-path '{transcript_output}' --batch-size '{args.batch_size}'"
                 
                 # Log verbose information
                 logging.debug(f"Processing file: {filename}")
